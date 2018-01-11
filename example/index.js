20b5f082c2c7b221bed35b636f3b904b152ecc08
Albino Tonnina <albinotonnina@gmail.com>
2018-01-11 13:41:09 +0000

    Fix issue related to https://github.com/parcel-bundler/parcel/issues/510

--- example/index.js
+++ example/index.js
@@ -1,40 +1,34 @@
-import chromeSvg from './svg/chrome.svg'
-import firefoxSvg from './svg/firefox.svg'
-import safariSvg from './svg/safari.svg'
-import edgeSvg from './svg/edge.svg'
-import operaSvg from './svg/opera.svg'
-import explorerSvg from './svg/explorer.svg'
-import dummySvg from './svg/dummy.svg'
+import chromeSvg from "./svg/chrome.svg";
+import firefoxSvg from "./svg/firefox.svg";
+import safariSvg from "./svg/safari.svg";
+import edgeSvg from "./svg/edge.svg";
+import operaSvg from "./svg/opera.svg";
+import explorerSvg from "./svg/explorer.svg";
+import dummySvg from "./svg/dummy.svg";
 
-function initComponent() {
-  // const parser = new DOMParser();
-  // const svgElement = parser.parseFromString(chromeSvg, "image/svg+xml");
+// const parser = new DOMParser();
+// const svgElement = parser.parseFromString(chromeSvg, "image/svg+xml");
 
-  const chromeElement = document.createElement("span");
-  const firefoxElement = document.createElement("span");
-  const safariElement = document.createElement("span");
-  const edgeElement = document.createElement("span");
-  const operaElement = document.createElement("span");
-  const explorerElement = document.createElement("span");
-  const dummyEl = document.createElement("span");
+const chromeElement = document.createElement("span");
+const firefoxElement = document.createElement("span");
+const safariElement = document.createElement("span");
+const edgeElement = document.createElement("span");
+const operaElement = document.createElement("span");
+const explorerElement = document.createElement("span");
+const dummyEl = document.createElement("span");
 
-  chromeElement.innerHTML = chromeSvg;
-  firefoxElement.innerHTML = firefoxSvg;
-  safariElement.innerHTML = safariSvg;
-  edgeElement.innerHTML = edgeSvg;
-  operaElement.innerHTML = operaSvg;
-  explorerElement.innerHTML = explorerSvg;
-  dummyEl.innerHTML = dummySvg;
-  
-  document.body.appendChild(chromeElement);
-  document.body.appendChild(firefoxElement);
-  document.body.appendChild(safariElement);
-  document.body.appendChild(edgeElement);
-  document.body.appendChild(operaElement);
-  document.body.appendChild(explorerElement);
-   document.body.appendChild(dummyEl);
-}
+chromeElement.innerHTML = chromeSvg;
+firefoxElement.innerHTML = firefoxSvg;
+safariElement.innerHTML = safariSvg;
+edgeElement.innerHTML = edgeSvg;
+operaElement.innerHTML = operaSvg;
+explorerElement.innerHTML = explorerSvg;
+dummyEl.innerHTML = dummySvg;
 
-document.addEventListener("DOMContentLoaded", function() {
-  initComponent();
-});
+document.body.appendChild(chromeElement);
+document.body.appendChild(firefoxElement);
+document.body.appendChild(safariElement);
+document.body.appendChild(edgeElement);
+document.body.appendChild(operaElement);
+document.body.appendChild(explorerElement);
+document.body.appendChild(dummyEl);
